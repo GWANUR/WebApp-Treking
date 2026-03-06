c6532060035f78829d9c7a0ea3bde5426e616e3f
KRUZKA_COFFEE <59487003+GWANUR@users.noreply.github.com>
2026-03-06 16:53:16 +0300

    Update Ready for use function

--- Tracking tasks/win_start.bat
+++ Tracking tasks/win_start.bat
@@ -1,18 +1,22 @@
 @echo off
-cd WebTreking
+cd /d "%~dp0"
 
-echo Getting local IPv4 address...
+echo Detecting local IPv4...
 
-for /f "tokens=14 delims= " %%i in ('ipconfig ^| findstr /i "IPv4"') do (
+for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /i "IPv4"') do (
 set IP=%%i
 goto :found
 )
 
 :found
-echo Your IP: %IP%
+set IP=%IP: =%
 
-echo Starting PHP server...
+echo.
+echo Starting server on http://%IP%:8000
+echo.
+
 start http://%IP%:8000
-php -S %IP%:8000
+
+WebTreking\php\php.exe -c WebTreking\php\php.ini -S %IP%:8000 -t WebTreking
 
 pause
